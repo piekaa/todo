e1583c6ad30f3480e1b4b0f4d1b6de804f0f521e
Piotr Iwaniuk <piekoszek@gmail.com>
2017-09-18 19:24:54 +0200

    Simple query

--- main.go
+++ main.go
@@ -2,10 +2,40 @@ package main
 
 import (
 	"fmt"
+	"github.com/todo/config"
 )
 
 func main(){
 
 	fmt.Println("Hello World!")
+	db,err := config.OpenDb()
+	defer db.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	rows, err := db.Query("select * from task")
+
+	for rows.Next() {
+		var id int
+		var description string
+		var done int
+		var name string
+		var parentTask int
+		err := rows.Scan(&id, &description, &done, &name, &parentTask)
+
+		if err != nil {
+			fmt.Println( err )
+		}
+
+		fmt.Println( id, description, done, name, parentTask)
+
+	}
+
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 
 }
